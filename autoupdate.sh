38f0e2d922873f8602495c04af1da18c8231f261
icaijy <caijunyi08@outlook.com>
2025-05-15 22:01:42 +1000

    autoupdate

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export TZ='Australia/Melbourne'
-LOGFILE=./cronlog.txt
+LOGFILE=~/icaijy.com/cronlog.txt
 echo "$(date) START" >> "$LOGFILE"
-python ./orac_tracker/scraper.py auto >> "$LOGFILE" 2>&1
+python ~/icaijy.com/orac_tracker/scraper.py auto >> "$LOGFILE" 2>&1
 echo -e "\n---\n" >> "$LOGFILE"
