0a65d3c00ddc1f3608217bd9aabad692e6ef6661
icaijy <caijunyi08@outlook.com>
2025-05-19 20:41:32 +1000

    auto update bug fixed

--- orac_tracker/apps.py
+++ orac_tracker/apps.py
@@ -1,3 +1,4 @@
+import os
 from django.apps import AppConfig
 
 class OracTrackerConfig(AppConfig):
@@ -5,20 +6,24 @@ class OracTrackerConfig(AppConfig):
     scheduler = None
 
     def ready(self):
-        # 仅注册 scheduler，不 start（避免数据库操作）
         from apscheduler.schedulers.background import BackgroundScheduler
         from django_apscheduler.jobstores import DjangoJobStore
         from . import tasks
 
+        if os.environ.get('RUN_MAIN', None) != 'true':
+            return  # 避免 runserver 启动两次
+
         scheduler = BackgroundScheduler()
         scheduler.add_jobstore(DjangoJobStore(), "default")
+
         scheduler.add_job(
             tasks.autoupdate,
             'cron',
             hour=4,
             minute=0,
             timezone='Australia/Melbourne',
-            id='daily_scraper_job'
+            id='daily_scraper_job',
+            replace_existing = True
         )
-
+        scheduler.start()
         OracTrackerConfig.scheduler = scheduler
