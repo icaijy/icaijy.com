574406c2e1a15aa36ae15a6c7b78be28c3b9349f
Cai Junyi <caijunyi08@outlook.com>
2026-08-19 13:00:28 +1000

    Add HOF comment moderation to admin

--- brainrot/admin.py
+++ brainrot/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
-from .models import HallOfFameEntry
+from .models import HallOfFameComment, HallOfFameEntry
 
 
 @admin.register(HallOfFameEntry)
@@ -35,3 +35,23 @@ class HallOfFameEntryAdmin(admin.ModelAdmin):
     @admin.action(description='Make selected entries private')
     def make_private(self, request, queryset):
         queryset.update(visibility=HallOfFameEntry.Visibility.PRIVATE)
+
+
+@admin.register(HallOfFameComment)
+class HallOfFameCommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'entry_link', 'short_body', 'is_submission_note', 'created_at')
+    list_filter = ('is_submission_note', 'created_at', 'entry__game_mode')
+    search_fields = ('author_name', 'user__username', 'body', 'entry__display_name', 'entry__user__username')
+    readonly_fields = ('entry', 'user', 'author_name', 'body', 'client_key', 'is_submission_note', 'created_at')
+
+    @admin.display(description='Author')
+    def author(self, obj):
+        return obj.public_name
+
+    @admin.display(description='HOF entry')
+    def entry_link(self, obj):
+        return format_html('<a href="/67/hall-of-fame/{}/">#{} · {}</a>', obj.entry_id, obj.entry_id, obj.entry.public_name)
+
+    @admin.display(description='Comment')
+    def short_body(self, obj):
+        return obj.body[:80]
