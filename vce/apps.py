b1f85cb2fe49795603b4b8ce96511398bd1473c0
Cai Junyi <caijunyi08@outlook.com>
2026-09-10 13:27:48 +1000

    Add VCE Algorithmics speedrun app config

---
+++ vce/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class VceConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "vce"
+    verbose_name = "VCE Algorithmics Speedrun"
